67414f07e5267ba1591246a4800b1f47e31805e8
simon <simonjustuswimmer@googlemail.com>
2018-08-08 17:18:16 +0200

    Finalze tests, extract .exe

--- SurrogateClient/MainWindow.xaml.cs
+++ SurrogateClient/MainWindow.xaml.cs
@@ -5,38 +5,28 @@ using OpenTok;
 using System.IO;
 using System.Net;
 
+
 namespace SurrogateClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    { 
 
-        Session _session;
-        Publisher _publisher;
+        private Session _session;
+        private Publisher _publisher;
+        private Subscriber _subscriber;
 
         public static readonly string URL = @"https://pscagebot.herokuapp.com/session";
 
         public MainWindow()
         {
-            InitializeComponent();
-            _publisher = new Publisher(Context.Instance, renderer: PublisherVideo, name: "drmueller");
-
-            JObject creds = getCredentials();
-            System.Diagnostics.Debug.WriteLine(String.Format("Connecting to server with: {0} {1} {2}",creds.GetValue("apiKey").ToString(), creds.GetValue("sessionId").ToString(), creds.GetValue("token").ToString()));
-            _session = new Session(Context.Instance, creds.GetValue("apiKey").ToString(), creds.GetValue("sessionId").ToString());
-
-            _session.Connected += Session_Connected;
-            _session.Disconnected += Session_Disconnected;
-            _session.Error += Session_Error;
-            _session.StreamReceived += Session_StreamReceived;
-
-            _session.Connect(creds.GetValue("token").ToString());
+            InitializeComponent();           
         }
 
-
-        private JObject getCredentials()
+        #region getCredentials
+        private JObject GetCredentials()
         {
             string html;
 
@@ -47,17 +37,19 @@ namespace SurrogateClient
             using (System.IO.Stream stream = response.GetResponseStream())
             using (StreamReader reader = new StreamReader(stream))
             {
-                //{"apiKey":"46088682","sessionId":"2_MX40NjA4ODY4Mn5-MTUyNTA4NDY0MjEzMX5mMzB3WTNuVHhReXFScngvSlFQNG1WcVd-fg","token":"T1==cGFydG5lcl9pZD00NjA4ODY4MiZzaWc9MTkyNTBiNWE2Mzc2MWQ2NWViYTA5MzExZmQxN2RiNWNmOTQxZWU3NTpzZXNzaW9uX2lkPTJfTVg0ME5qQTRPRFk0TW41LU1UVXlOVEE0TkRZME1qRXpNWDVtTXpCM1dUTnVWSGhSZVhGU2NuZ3ZTbEZRTkcxV2NWZC1mZyZjcmVhdGVfdGltZT0xNTI1MDg1NDgwJnJvbGU9cHVibGlzaGVyJm5vbmNlPTE1MjUwODU0ODAuOTczNjk2OTAwNjQw"}
+                
 
                 html = reader.ReadToEnd();
             }
 
             return JObject.Parse(html);
         }
+        #endregion getCredentials
 
         private void Session_Connected(object sender, EventArgs e)
         {
             Console.WriteLine("Connected to session.");
+            _session.Publish(_publisher);
         }
 
         private void Session_Disconnected(object sender, EventArgs e)
@@ -70,6 +62,7 @@ namespace SurrogateClient
             System.Diagnostics.Debug.WriteLine("Session error:" + e.ErrorCode);
         }
 
+
         /// <summary>
         /// When another client publishes a stream to a session, the Session.StreamReceived message is sent and this method is called
         /// </summary>
@@ -77,8 +70,47 @@ namespace SurrogateClient
         /// <param name="e"></param>
         private void Session_StreamReceived(object sender, Session.StreamEventArgs e)
         {
-            Subscriber subscriber = new Subscriber(Context.Instance, e.Stream, SubscriberVideo);
-            _session.Subscribe(subscriber);
+            if(_subscriber != null)
+            {
+                _session.Unsubscribe(_subscriber);
+                _subscriber.Dispose();
+            }
+
+            System.Diagnostics.Debug.WriteLine("Stream received: "+e.Stream.Name);
+            _subscriber = new Subscriber(Context.Instance, e.Stream, SubscriberVideo);
+            _session.Subscribe(_subscriber);
+        }
+
+        /// <summary>
+        /// Called if the user presses the connect button. Creates a session and connects to the opentok cloud
+        /// </summary>
+        /// <param name="sender">not needed</param>
+        /// <param name="e">not needed</param>
+        private void ConnectClicked(object sender, RoutedEventArgs e)
+        {
+            _publisher = new Publisher(Context.Instance, renderer: PublisherVideo, name: "drmueller");
+
+            JObject creds = GetCredentials();
+            System.Diagnostics.Debug.WriteLine(String.Format("Connecting to server with: {0} {1} {2}", creds.GetValue("apiKey").ToString(), creds.GetValue("sessionId").ToString(), creds.GetValue("token").ToString()));
+            _session = new Session(Context.Instance, creds.GetValue("apiKey").ToString(), creds.GetValue("sessionId").ToString());
+
+            _session.Connected += Session_Connected;
+            _session.Disconnected += Session_Disconnected;
+            _session.Error += Session_Error;
+            _session.StreamReceived += Session_StreamReceived;
+
+            _session.Connect(creds.GetValue("token").ToString());
+        }
+
+        /// <summary>
+        /// Disconnects from the session
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DisconnectClicked(object sender, RoutedEventArgs e)
+        {
+            _session.Disconnect();
+            _subscriber = null;
         }
     }
 }
